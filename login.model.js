5bab84b7bd7c27111bd712bef202825821633ff7
ducngo123456 <135568147+duc-ngo-saltovn@users.noreply.github.com>
2023-10-18 13:42:47 +0700

    #16 - create file 6-10 conflict

--- login.model.js
+++ login.model.js
@@ -1,2 +1,7 @@
 console.log("Create Login Model");
 console.log("Login");
+console.log("conflict 6");
+console.log("conflict 7");
+console.log("conflict 8");
+console.log("conflict 9");
+console.log("conflict 10");
