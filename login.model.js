8c6d2a5b3d3ce3b067612a5531ad7f49cafcd638
ducngo123456 <135568147+duc-ngo-saltovn@users.noreply.github.com>
2023-10-19 14:23:37 +0700

    #19 - commit first

--- login.model.js
+++ login.model.js
@@ -10,3 +10,8 @@ console.log("conflict 2");
 console.log("conflict 3");
 console.log("conflict 4");
 console.log("conflict 5");
+console.log("conflict 11");
+console.log("conflict 12");
+console.log("conflict 13");
+console.log("conflict 14");
+console.log("conflict 15");
