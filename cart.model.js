0a41896b59df4bd4bbc0bb6589d3af9244850dbd
ducngo123456 <135568147+duc-ngo-saltovn@users.noreply.github.com>
2023-10-20 09:27:32 +0700

    '#19 - last'

--- cart.model.js
+++ cart.model.js
@@ -4,3 +4,8 @@ console.log("conflict 2");
 console.log("conflict 3");
 console.log("conflict 4");
 console.log("conflict 5");
+console.log("conflict 6");
+console.log("conflict 7");
+console.log("conflict 8");
+console.log("conflict 9");
+console.log("conflict 10");
